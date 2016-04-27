f5f37d2a3bfaf62de21230a56b74e45e823d9412
SamirAbuHadid <samir.abuhadid@gmail.com>
2016-04-27 21:00:44 +0300

    Made app alive due to EF hack

--- MStrudel.Domain/Concrete/EFDbContext.cs
+++ MStrudel.Domain/Concrete/EFDbContext.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using MStrudel.Domain.Entities;
 
 namespace MStrudel.Domain.Concrete
 {
 	public class EFDbContext : DbContext
 	{
+		static EFDbContext()
+		{
+			var ensureDLLIsCopied = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+		}
+
 		public DbSet<Product> Products { get; set; }
 		public DbSet<Category> Categories { get; set; }
 		public DbSet<Order> Orders { get; set; }
